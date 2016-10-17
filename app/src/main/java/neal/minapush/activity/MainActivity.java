474d51d5621a9bad74faa7deb8c6fcd49c61f9c6
dongxinlei <627007915@qq.com>
2016-10-17 17:36:59 +0800

    2ss

--- app/src/main/java/neal/minapush/activity/MainActivity.java
+++ app/src/main/java/neal/minapush/activity/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //sssssssss
         System.out.println("activity start"+android.os.Process.myPid()+'-'+android.os.Process.myTid());
     }
 
